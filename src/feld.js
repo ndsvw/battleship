33bd7a9e58f547a34c224d6f987f868c6a9a7eb4
Alexander Lübeck <omega.zero@outlook.com>
2017-09-23 07:37:43 +0200

    Jedes Array mit Schiff-Positionen wird zu Beginn der Überprüfung aufsteigend sortiert.

--- src/feld.js
+++ src/feld.js
@@ -54,6 +54,17 @@ module.exports = class Feld {
 
   checkShipArray(arr) {
 
+    // sort ascending
+    for (let i = 0; i < arr.length - 1; i++) {
+      for (let j = i; j >= 0; j--) {
+        if (arr[j] > arr[j + 1]) {
+          let tmp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = tmp;
+        }
+      }
+    }
+
     // Prüfen, ob alle Schiffe platziert wurden
     if (arr.length != this.SHIPPOSCOUNTER) {
       return {
