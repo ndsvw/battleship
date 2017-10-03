197e4869a7bf7df2fd25ab83ab604b86712159c3
Alexander Lübeck <omega.zero@outlook.com>
2017-10-03 09:07:02 +0200

    using Javascript's Array.sort()

--- src/feld.js
+++ src/feld.js
@@ -55,15 +55,7 @@ module.exports = class Feld {
   checkShipArray(arr) {
 
     // sort ascending
-    for (let i = 0; i < arr.length - 1; i++) {
-      for (let j = i; j >= 0; j--) {
-        if (arr[j] > arr[j + 1]) {
-          let tmp = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = tmp;
-        }
-      }
-    }
+    arr.sort();
 
     // Prüfen, ob alle Schiffe platziert wurden
     if (arr.length != this.SHIPPOSCOUNTER) {
