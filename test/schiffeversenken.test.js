6e67930c16aa5dc2826a3e491a314bedfe5ed656
Alex Lübeck <omega.zero@outlook.com>
2018-02-09 16:48:07 +0100

    schiffeversenken.test simplified

--- test/schiffeversenken.test.js
+++ test/schiffeversenken.test.js
@@ -9,20 +9,10 @@ createExampleSchiffeversenken = () => {
   s.addPlayer("playerID1");
   s.addPlayer("playerID2");
 
-  let ships = [];
-  ships = ships.concat([0, 1, 2, 3, 4]);
-  ships = ships.concat([7, 8, 9]);
-  ships = ships.concat([20, 21, 22, 23]);
-  ships = ships.concat([41, 51, 61]);
-  ships = ships.concat([55, 65]);
+  let ships = [...[0, 1, 2, 3, 4], ...[7, 8, 9], ...[20, 21, 22, 23], ...[41, 51, 61], ...[55, 65]];
   s.getPlayerById("playerID1").feld.setShips(ships);
 
-  ships = [];
-  ships = ships.concat([67, 77]);
-  ships = ships.concat([31, 41, 51, 61, 71]);
-  ships = ships.concat([0, 1, 2]);
-  ships = ships.concat([16, 17, 18]);
-  ships = ships.concat([96, 97, 98, 99]);
+  ships = [...[67, 77], ...[31, 41, 51, 61, 71], ...[0, 1, 2], ...[16, 17, 18], ...[96, 97, 98, 99]];
   s.getPlayerById("playerID2").feld.setShips(ships);
 
   return s;
@@ -50,22 +40,12 @@ describe('schiffeversenken...', () => {
     s.startTheGame();
     s.whoseTurn = "playerID1";
 
-    s.shoot("playerID1", 0);
-    s.shoot("playerID1", 1);
-    s.shoot("playerID1", 2);
-    s.shoot("playerID1", 16);
-    s.shoot("playerID1", 17);
-    s.shoot("playerID1", 18);
-    s.shoot("playerID1", 31);
-    s.shoot("playerID1", 41);
-    s.shoot("playerID1", 51);
-    s.shoot("playerID1", 61);
-    s.shoot("playerID1", 71);
-    s.shoot("playerID1", 67);
-    s.shoot("playerID1", 77);
-    s.shoot("playerID1", 96);
-    s.shoot("playerID1", 97);
-    s.shoot("playerID1", 98);
+    let shotArray = [0, 1, 2, 16, 17, 18, 31, 41, 51, 61, 71, 67, 77, 96, 97, 98];
+
+    shotArray.map(pos => {
+      s.shoot("playerID1", pos);
+    });
+
     let res = s.shoot("playerID1", 99);
 
     res.gameOver.should.be.equal(true);
@@ -83,16 +63,10 @@ describe('schiffeversenken...', () => {
     s.addPlayer("playerID1");
     s.addPlayer("playerID2");
 
-    let ships = [];
-    ships = ships.concat([0, 1]);
-    ships = ships.concat([8, 9]);
-    ships = ships.concat([23, 24]);
+    let ships = [...[0, 1], ...[8, 9], ...[23, 24]];
     s.getPlayerById("playerID1").feld.setShips(ships);
 
-    ships = [];
-    ships = ships.concat([1, 2]);
-    ships = ships.concat([15, 16]);
-    ships = ships.concat([8, 9]);
+    ships = [...[1, 2], ...[15, 16], ...[8, 9]];
     s.getPlayerById("playerID2").feld.setShips(ships);
 
     s.startTheGame()
@@ -143,20 +117,10 @@ describe('schiffeversenken.startTheGame()', () => {
     s.addPlayer("playerID1");
     s.addPlayer("playerID2");
 
-    let ships = [];
-    ships = ships.concat([0, 1, 2, 3, 4]);
-    ships = ships.concat([7, 8, 9]);
-    ships = ships.concat([20, 21, 22, 23]);
-    ships = ships.concat([41, 51, 61]);
-    ships = ships.concat([55, 65]);
+    let ships = [...[0, 1, 2, 3, 4], ...[7, 8, 9], ...[20, 21, 22, 23], ...[41, 51, 61], ...[55, 65]];
     s.getPlayerById("playerID1").feld.setShips(ships);
 
-    ships = [];
-    ships = ships.concat([67, 77]);
-    ships = ships.concat([31, 41, 51, 61, 71]);
-    ships = ships.concat([0, 1, 2]);
-    ships = ships.concat([16, 17, 18]);
-    ships = ships.concat([96, 97, 98, 99]);
+    ships = [...[67, 77], ...[31, 41, 51, 61, 71], ...[0, 1, 2], ...[16, 17, 18], ...[96, 97, 98, 99]];
     s.getPlayerById("playerID2").feld.setShips(ships);
 
     s.startTheGame().status.should.be.equal("success");
